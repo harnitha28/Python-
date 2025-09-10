3a609cf67f75e534ce90c6dacf47fd706a037130
harnitha28 <harnithasuresh28@gmail.com>
2025-09-10 17:35:57 +0530

    Add files via upload

---
+++ arthimetic program.py
@@ -0,0 +1,10 @@
+x=int(input("Enter your number:"))
+y=int(input("Enter your number:"))
+add=x+y
+sub=x-y
+multi=x*y
+divi=x/y
+print("addition is ",add)
+print("subtraction is",sub)
+print("multiplication is",multi)
+print("division is ",divi)

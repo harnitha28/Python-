f9b94d4e19da0355269947b8c4ec67b8bc28857f
harnitha28 <harnithasuresh28@gmail.com>
2025-09-11 17:35:59 +0530

    Add files via upload

---
+++ if elif else (traffic signal).py
@@ -0,0 +1,10 @@
+colour=input("Enter the colour:")
+if colour=="red":
+    print("stop ur vehicle")
+elif colour=="yellow":
+    print("get ready")
+elif colour=="green":
+    print("you may go")
+else:
+    print("no msg")
+    

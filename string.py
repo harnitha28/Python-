ffa48b6c3dfec3a2a325bf4da2e45a802fec4edd
harnitha28 <harnithasuresh28@gmail.com>
2025-09-13 19:31:09 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,22 @@
+name1= "kevin james"
+print("String is:", name1)
+name2 = "tanisha"
+print("First character:", name2[0])
+print("Last character:", name2[-1])
+
+subject = "ComputerSciencewithai"
+print("First 3 letters:", subject[:6])
+print("Last 3 letters:", subject[-5:])
+print("Middle part:", subject[1:7])
+
+word1 = "Hello"
+word2 = "World"
+result = word1 + " " + word2
+print("the concatenated string is :", result)
+
+program = "Python"
+print("Original:", program)
+print("Reversed:", program[::-1])
+
+sentence = "Hello, World! Welcome to Idle"
+print("Length of string:", len(sentence))
---
+++ tuple.py
@@ -0,0 +1,10 @@
+fruits = ("apple", "banana", "cherry", "mango", "orange")
+print("Tuple Created:", fruits)
+print( fruits[0])   
+print(fruits[-1])
+print(fruits[3])
+print("First three fruits:", fruits[0:3])   
+print("From second to last:", fruits[1:])   
+for item in fruits:
+    print(item)
+

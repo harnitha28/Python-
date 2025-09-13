cfd17bb5ac6d402bdd39147863a6c6d5660770ec
harnitha28 <harnithasuresh28@gmail.com>
2025-09-13 19:40:02 +0530

    Add files via upload

--- list.py
+++ list.py
@@ -1,5 +1,10 @@
-a=[11,22,33,44,55,66]
-print(a[1]+a[2])
-print(a[-1]+a[3])
-print(a[-2]+a[1])
-print(a[-3]+a[-2])
+numbers = [10, 20, 30, 40, 50]
+print("List of Numbers:", numbers)
+fruits = ["apple", "banana", "cherry", "mango"]
+print("List of Strings:", fruits)
+data = ["sidhara,18, bsc cs with ai"]
+print(data)
+values = [[1, 2, 3], ["a", "b", "c"], [10.5, 20.5]]
+print(values)
+list_marks = [('hari', "87"), ('tani', "94"), ('kavi', "88")]
+print(list_marks)

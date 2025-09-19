109a9731edd25a98ef89e9c9a353238204d8bc6f
harnitha28 <harnithasuresh28@gmail.com>
2025-09-19 16:29:18 +0530

    marks.py

--- marks.py
+++ marks.py
@@ -6,17 +6,35 @@ class stud:
          self.mark1=m1
          self.mark2=m2
          self.mark3=m3
-    def calculate:
-        tot=m1+m2+m3
-        avg=tot/3
-        print("total marks=",tot)
-        print("average=",avg)
+    def calculate(self):
+        self.tot=self.mark1+self.mark2+self.mark3
+        self.avg=self.tot/3
+        print("total marks=",self.tot)
+        print("average=",self.avg)
         if m1>=40 and m2>=40 and m3>=40:
             self.result="pass"
         else:
             self.result="fail"
-     def display(self):
-         print("student no:",self.no)
+            
+    def display(self):
+         print("student no:",self.sno)
+         print("student name:",self.sname)
+         print("student age:",self.sage)
+         print("mark1=",self.mark1)
+         print("mark2=",self.mark2)
+         print("mark3=",self.mark3)
+         print("total=",self.tot)
+         print("average=",self.avg)
+         print("result=",self.result)
          
-x=int(input("enter  roll"))
-y=input("            
+x=int(input("Enter  roll"))
+y=input("Enter a name:")
+z=int(input("Enter age:"))
+m1=int(input("enter mark1="))
+m2=int(input("enter mark2="))
+m3=int(input("enter mark3="))
+
+obj=stud(x,y,z,m1,m2,m3)
+obj.calculate()
+obj.display()
+
